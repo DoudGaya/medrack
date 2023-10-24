ecad81057156e301a9a48648bfb5f3ce8dbcd571
DoudGaya <arbdoule@yahoo.com>
2023-10-24 17:26:05 +0100

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import med from '../public/medcine.jpg'
+import logo from '../public/logo.jpg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <main className="flex min-h-screen flex-col space-y-4 items-center  p-24">
+      {/* <Image src={med.src} className=' h-[230px] w-[230px] rounded-full border' alt="" width={1000} height={1000} /> */}
+      <Image src={logo.src} className=' h-[200px] w-[300px] object-cover object-center' alt="" width={1000} height={1000} />
+      <h2 className=' md:text-5xl text-3xl'>Coming Soon. </h2>
     </main>
   )
 }
