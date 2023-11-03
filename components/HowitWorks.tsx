669cb6be36e97a9e3b9aac4155b3ee9afd2446e0
DoudGaya <arbdoule@yahoo.com>
2023-11-03 19:08:42 +0100

    key prop

--- components/HowitWorks.tsx
+++ components/HowitWorks.tsx
@@ -14,7 +14,7 @@ const works = [
 
     },
      {
-        id: 1,
+        id: 2,
         icon: (
            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16 stroke-primary">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m6.75 12l-3-3m0 0l-3 3m3-3v6m-1.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
@@ -26,7 +26,7 @@ const works = [
 
     },
     {
-        id: 1,
+        id: 3,
         icon: (
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16 stroke-primary">
                  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
@@ -38,7 +38,7 @@ const works = [
 
     },
     {
-        id: 1,
+        id: 4,
         icon: (
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16 stroke-primary">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
@@ -59,7 +59,7 @@ export const HowitWorks = () => {
                   {
                     works.map(( item: any) => {
                         return (
-                            <div className=" flex py-10 flex-col items-center justify-center space-y-5 text-center">
+                            <div key={item.id} className=" flex py-10 flex-col items-center justify-center space-y-5 text-center">
                                 <span className=' bg-yellow-300 hover:bg-brand-green cursor-pointer p-4 rounded-full'>{item.icon}</span>
                                 <div className=" px-16 lg:px-6">
                                      <p className=' font-semibold font-quicksand text-lg'>{item.message}</p>
