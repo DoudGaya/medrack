9b01deef7bb6a1dc3fe4b68edcdeef0af74e8b2f
DoudGaya <arbdoule@yahoo.com>
2023-11-24 09:05:45 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,29 @@ import { HomeAbout } from "@/components/HomeAbout"
 import { HowitWorks } from "@/components/HowitWorks"
 import { Navbar } from "@/components/Navbar"
 import { Showcase } from "@/components/Showcase"
+import coming from '@/public/img/coming_soon.jpg'
 
 
 export default function Home() {
   return (
-    <main className=" flex w-full flex-col">
-      <Navbar />
-      <Banner />
-      <div className="">
-        <HomeAbout />
-      </div>
-      <div className="">
-        <Showcase />
-      </div>
-      <div className="">
-        <HowitWorks />
-      </div>
-      <div className="">
-        <FAQ />
-      </div>
-    </main>
+    <div className=" bg-cover bg-center h-screen w-full" style={{backgroundImage: `url(${coming.src})`}}>
+
+    </div>
+    // <main className=" flex w-full flex-col">
+    //   <Navbar />
+    //   <Banner />
+    //   <div className="">
+    //     <HomeAbout />
+    //   </div>
+    //   <div className="">
+    //     <Showcase />
+    //   </div>
+    //   <div className="">
+    //     <HowitWorks />
+    //   </div>
+    //   <div className="">
+    //     <FAQ />
+    //   </div>
+    // </main>
   )
 }
